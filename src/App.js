2ea22fdf6783ca2ac887b4194df4199b1fa5fff0
gomtak <tak6831@gmail.com>
2022-08-16 22:45:40 +0900

    :tada: init klay-project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import logo from './logo.svg';
+import Caver from 'caver-js'
 import './App.css';
 
+const COUNT_CONTRACT_ADDRESS = '0xF1e739BF8029396621FcEEED5fd662442cafd4fB';
+const ACCESS_KEY_ID = 'KASKT5I74FJOYN4J6LBJQUAR';
+const SECRET_ACCESS_KEY = 'MlmGP2l9XauOBCUNDP_BYax2sAN51bqSOs-bkHoX';
+const CHAIN_ID = '1001'; // MAINNET 8217 TESTNET 1001
+const COUNT_ABI = [ { "constant": true, "inputs": [], "name": "count", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "getBlockNumber", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_count", "type": "uint256" } ], "name": "setCount", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" } ];
+
+const option = {
+  headers: [
+    {
+      name: "Authorization",
+      value:"Basic " + Buffer.from(ACCESS_KEY_ID + ":" + SECRET_ACCESS_KEY).toString("base64")
+    },
+    {name:"x-chain-id", value: CHAIN_ID}
+  ]
+}
+
+const caver = new Caver(new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", option));
+const CountContract = new caver.contract(COUNT_ABI, COUNT_CONTRACT_ADDRESS);
+const readCount = async () => {
+  const _count = await CountContract.methods.count().call();
+  console.log(_count);
+}
+// 1 Smart contract 배포 주소 파악(가져오기)
+// 2 caver.js 이용해서 스마트 컨트랙트 연동하기
+// 3 가져온 스마트 컨트렉트 실행 결과(데이터) 웹에 표현하기
+
 function App() {
+  readCount();
   return (
     <div className="App">
       <header className="App-header">
